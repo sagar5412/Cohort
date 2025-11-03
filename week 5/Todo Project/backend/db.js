e8746ff702d7aa82e6d3e85cad60e0e057d344d8
sagar5412 <sagar.gk.bly@gmail.com>
2025-11-03 22:37:45 +0530

    docker

--- week 5/Todo Project/backend/db.js
+++ week 5/Todo Project/backend/db.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://sagargkbly_db_user:5FULAaNySIW5PHOy@cluster0.viw2d0d.mongodb.net/TodoApp')
+mongoose
+  .connect("mongodb://mongo:27017/myDatabase")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Mongodb connection error:", err));
 
 const todoSchema = new mongoose.Schema({
-    title:String,
-    description:String,
-    completed:Boolean
-})
+  title: String,
+  description: String,
+  completed: Boolean,
+});
 
-const todo = mongoose.model('todos',todoSchema);
+const todo = mongoose.model("todos", todoSchema);
 
 module.exports = {
-    todo
-}
+  todo,
+};
